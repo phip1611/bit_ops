9e4dbb593d3288cc515077c110db06c4950bbe28
Philipp Schuster <phip1611@gmail.com>
2024-07-13 16:25:00 +0200

    trait_api: add inline and must_use attributes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+# v0.1.5 - 2024-07-13
+
+- Trait API: add `#[inline]` and `#[must_use]` to each function impl
+
 # v0.1.1, v0.1.2, v0.1.3, v0.1.4 - 2024-07-13
 
 - Doc fixes
--- src/trait_api/macros.rs
+++ src/trait_api/macros.rs
@@ -30,6 +30,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::set_bit`]: crate::bitops_", stringify!($primitive_ty), "::set_bit")]
+            #[inline]
+            #[must_use]
             fn set_bit(self, bit: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::set_bit(self, bit)
@@ -40,6 +42,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::clear_bit`]: crate::bitops_", stringify!($primitive_ty), "::clear_bit")]
+            #[inline]
+            #[must_use]
             fn clear_bit(self, bit: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::clear_bit(self, bit)
@@ -50,6 +54,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::is_set`]: crate::bitops_", stringify!($primitive_ty), "::is_set")]
+            #[inline]
+            #[must_use]
             fn is_set(self, bit: Self) -> bool {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::is_set(self, bit)
@@ -60,6 +66,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::toggle`]: crate::bitops_", stringify!($primitive_ty), "::toggle")]
+            #[inline]
+            #[must_use]
             fn toggle(self, bit: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::toggle(self, bit)
@@ -70,6 +78,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::set_bits`]: crate::bitops_", stringify!($primitive_ty), "::set_bits")]
+            #[inline]
+            #[must_use]
             fn set_bits(self, value: Self, value_bits: Self, value_shift: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::set_bits(self, value, value_bits, value_shift)
@@ -80,6 +90,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::set_bits_n`]: crate::bitops_", stringify!($primitive_ty), "::set_bits_n")]
+            #[inline]
+            #[must_use]
             fn set_bits_n(self, ops: &[(Self, Self, Self)]) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::set_bits_n(self, ops)
@@ -90,6 +102,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::set_bits_exact`]: crate::bitops_", stringify!($primitive_ty), "::set_bits_exact")]
+            #[inline]
+            #[must_use]
             fn set_bits_exact(self, value: Self, value_bits: Self, value_shift: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::set_bits_exact(self, value, value_bits, value_shift)
@@ -100,6 +114,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::set_bits_exact_n`]: crate::bitops_", stringify!($primitive_ty), "::set_bits_exact_n")]
+            #[inline]
+            #[must_use]
             fn set_bits_exact_n(self, ops: &[(Self, Self, Self)]) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::set_bits_exact_n(self, ops)
@@ -110,6 +126,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::clear_bits`]: crate::bitops_", stringify!($primitive_ty), "::clear_bits")]
+            #[inline]
+            #[must_use]
             fn clear_bits(self, clear_mask: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::clear_bits(self, clear_mask)
@@ -120,6 +138,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::highest_bit`]: crate::bitops_", stringify!($primitive_ty), "::highest_bit")]
+            #[inline]
+            #[must_use]
             fn highest_bit(self) -> Option<Self> {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::highest_bit(self)
@@ -130,6 +150,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::lowest_bit`]: crate::bitops_", stringify!($primitive_ty), "::lowest_bit")]
+            #[inline]
+            #[must_use]
             fn lowest_bit(self) -> Option<Self> {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::lowest_bit(self)
@@ -140,6 +162,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::get_bits`]: crate::bitops_", stringify!($primitive_ty), "::get_bits")]
+            #[inline]
+            #[must_use]
             fn get_bits(self, value_bits: Self, value_shift: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::get_bits(self, value_bits, value_shift)
@@ -150,6 +174,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::create_mask`]: crate::bitops_", stringify!($primitive_ty), "::create_mask")]
+            #[inline]
+            #[must_use]
             fn create_mask(bits: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::create_mask(bits)
@@ -160,6 +186,8 @@ macro_rules! impl_trait {
             #[doc = concat!("but as associated function (method) on `", stringify!($primitive_ty), "`.")]
             #[doc = ""] // newline needed so that markdown links work
             #[doc = concat!("[`bitops_", stringify!($primitive_ty), "::create_shifted_mask`]: crate::bitops_", stringify!($primitive_ty), "::create_shifted_mask")]
+            #[inline]
+            #[must_use]
             fn create_shifted_mask(bits: Self, lshift: Self) -> Self {
                 paste::paste! {
                     $crate::[< bitops _ $primitive_ty >]::create_shifted_mask(bits, lshift)

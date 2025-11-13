dfae504f81ffbb1310695f6d36b647ca84757001
Philipp Schuster <phip1611@gmail.com>
2025-11-13 14:07:48 +0100

    Revert "xxx: add Julian's impl for comparision"
    This reverts commit 4a18fa9bc7d4d69583c662f836b22534cfd1e1c8.

--- benches/bitmap_iter.rs
+++ benches/bitmap_iter.rs
@@ -2,78 +2,6 @@ use bit_ops::BitmapIter;
 use criterion::{Criterion, criterion_group, criterion_main};
 use rand::Rng;
 use std::hint::black_box;
-use std::ops::Mul;
-
-/// An iterator that turns a sequence of u64s into a sequence of bit positions
-/// that are set.
-///
-/// This is useful to iterate over dirty memory bitmaps.
-struct BlitzBitposIterator<I> {
-    underlying_it: I,
-
-    /// How many `u64`'s we've already consumed.
-    ///
-    /// `u32` is sufficient.
-    word_pos: u32,
-
-    /// If we already started working on a u64, it's here. Together with the bit
-    /// position where we have to continue.
-    current_word: Option<(u64 /* cur word */, u32 /* cur pos */)>,
-}
-
-impl<I> Iterator for BlitzBitposIterator<I>
-where
-    I: Iterator<Item = u64>,
-{
-    type Item = u64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if self.current_word.is_none() {
-                self.current_word = self.underlying_it.next().map(|w| (w, 0));
-            }
-
-            let (word, word_bit) = self.current_word?;
-
-            // Continue early if there is no chance to find something.
-            if word != 0 && word_bit < 64 {
-                let shifted_word = word >> word_bit;
-                if shifted_word != 0 {
-                    let zeroes = shifted_word.trailing_zeros();
-
-                    self.current_word = Some((word, zeroes + word_bit + 1));
-                    let next_bitpos = (self.word_pos as u64)
-                        .mul(64)
-                        // the inner value can not overflow
-                        .checked_add(word_bit as u64 + zeroes as u64)
-                        .unwrap();
-
-                    return Some(next_bitpos);
-                }
-            }
-
-            self.current_word = None;
-            self.word_pos += 1;
-        }
-    }
-}
-
-
-
-pub trait BitposIteratorExt: Iterator<Item = u64> + Sized {
-    /// Turn an iterator over `u64` into an iterator over the bit positions of
-    /// all 1s. We basically treat the incoming `u64` as one gigantic integer
-    /// and just spit out which bits are set.
-    fn bit_positions(self) -> impl Iterator<Item = u64> {
-        BlitzBitposIterator {
-            underlying_it: self,
-            word_pos: 0,
-            current_word: None,
-        }
-    }
-}
-
-impl<I: Iterator<Item = u64> + Sized> BitposIteratorExt for I {}
 
 fn get_random_u8(ones_percent: u8) -> u8 {
     assert!(ones_percent <= 100);
@@ -203,53 +131,6 @@ fn bench_bitmap_iter(c: &mut Criterion) {
             for x in iter {
                 let _ = black_box(x);
             }
-        })
-    });
-
-
-    c.bench_function("blitz_bitmap_iter_u64_0%ones", |b| {
-        let bitmap = get_random_bitmap_u64(0);
-        b.iter(|| {
-            let iter = black_box(bitmap.as_ref().iter().copied().bit_positions());
-            for x in iter {
-                let _ = black_box(x);
-            };
-        })
-    });
-    c.bench_function("blitz_bitmap_iter_u64_10%ones", |b| {
-        let bitmap = get_random_bitmap_u64(10);
-        b.iter(|| {
-            let iter = black_box(bitmap.as_ref().iter().copied().bit_positions());
-            for x in iter {
-                let _ = black_box(x);
-            };
-        })
-    });
-    c.bench_function("blitz_bitmap_iter_u64_40%ones", |b| {
-        let bitmap = get_random_bitmap_u64(40);
-        b.iter(|| {
-            let iter = black_box(bitmap.as_ref().iter().copied().bit_positions());
-            for x in iter {
-                let _ = black_box(x);
-            };
-        })
-    });
-    c.bench_function("blitz_bitmap_iter_u64_70%ones", |b| {
-        let bitmap = get_random_bitmap_u64(70);
-        b.iter(|| {
-            let iter = black_box(bitmap.as_ref().iter().copied().bit_positions());
-            for x in iter {
-                let _ = black_box(x);
-            };
-        })
-    });
-    c.bench_function("blitz_bitmap_iter_u64_99%ones", |b| {
-        let bitmap = get_random_bitmap_u64(99);
-        b.iter(|| {
-            let iter = black_box(bitmap.as_ref().iter().copied().bit_positions());
-            for x in iter {
-                let _ = black_box(x);
-            };
         })
     });
 }
